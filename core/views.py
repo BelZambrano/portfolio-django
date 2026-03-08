a2b692fb3cce10970e51c189743ea2599112c8e9
BelZambrano <bzambrano888@gmail.com>
2026-03-08 15:24:02 -0300

    Primer portfolio Django funcionando

---
+++ core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "core/home.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
+
+
+def projects(request):
+    return render(request, "core/projects.html")
+
+
+def contact(request):
+    return render(request, "core/contact.html")
